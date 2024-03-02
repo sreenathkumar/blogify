aeb61f60bbf73bafa496f191247c15aafe621e6b
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-03-02 12:58:01 +0600

    Feat: made a separate component for avatar image with author name

---
+++ src/components/AvatarImage.jsx
@@ -0,0 +1,8 @@
+
+export default function AvatarImage({name}) {
+  return (
+    <div className="avater-img bg-orange-600 text-white">
+        <span className="">{name[0]}</span>
+      </div>
+  )
+}
