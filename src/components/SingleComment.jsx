a7ed54471968bc33287bb889609e45e25f44f672
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-03-02 12:49:04 +0600

    Feat: made dynamic with default prop value

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,21 +1,13 @@
-export default function SingleComment() {
+import AvatarImage from "./AvatarImage";
+
+export default function SingleComment({authorName='Author Name', comment="Comment Content"}) {
   return (
     <div className="flex items-start space-x-4 my-8">
-      <div className="avater-img bg-orange-600 text-white">
-        <span className="">S</span>
-      </div>
+      <AvatarImage name={authorName[0]}/>
       <div className="w-full">
-        <h5 className="text-slate-500 font-bold">Saad Hasan</h5>
+        <h5 className="text-slate-500 font-bold">{authorName}</h5>
         <p className="text-slate-300">
-          Today I was mob programming with Square&#39;s Mobile & Performance
-          Reliability team and we toyed with an interesting idea. Our codebase
-          has classes that represent screens a user can navigate to. These
-          classes are defined in modules, and these modules have an owner team
-          defined. When navigating to a screen, we wanted to have the owner team
-          information available, at runtime. We created a build tool that looks
-          at about 1000 Screen classes, determines the owner team, and generates
-          a class to do the lookup at runtime. The generated code looked like
-          this:
+          {comment}
         </p>
       </div>
     </div>
