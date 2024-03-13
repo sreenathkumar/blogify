634dc46a78dcbe3ea13bf79fd857e35a1ed0df6a
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-03-13 19:14:56 +0600

    Feat: added home button to redirect to home

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,9 @@
 import errorImage from "@assets/cry.png";
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
+  const navigate = useNavigate();
 
   return (
     <section className="container">
@@ -15,6 +16,15 @@ export default function ErrorPage() {
               {`Don't Cry. It's just a ${error.status} Error !`}
             </p>
             <p className="text-xl">{error.statusText || error.message}</p>
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                navigate("/");
+              }}
+              className="bg-indigo-600 w-auto text-white px-6 py-2 mt-4 self-center  md:py-3 rounded-md hover:bg-indigo-700 transition-all duration-200"
+            >
+              Go to Home
+            </button>
           </div>
         </div>
       </div>
