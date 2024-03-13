709464d4afc2c08465ca2f1b761fa1d16661cb4a
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-03-13 19:03:10 +0600

    Fix: fixed error when image name is not provided

--- src/utils/getImage.js
+++ src/utils/getImage.js
@@ -2,6 +2,9 @@
 //name - image name
 //type - image type (i.e. blog, avatar)
 export const getImage = (name, type) => {
+  if (!name) {
+    return null;
+  }
   if (type === "avatar") {
     return `${import.meta.env.VITE_API_URL}/uploads/avatar/${name}`;
   }
